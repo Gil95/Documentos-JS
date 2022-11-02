47966a7c01fd69a26a16938cd37682041d2724d2
Gil95 <gilsongomesneto.neto@gmail.com>
2022-11-02 18:41:29 -0300

    Update objeto01.js

--- Curso JS/aula17/exercícios object/objeto01.js
+++ Curso JS/aula17/exercícios object/objeto01.js
@@ -1,9 +1,10 @@
-let amigo = {nome: 'José',
-sexo: 'M',
-peso:85.4,
-engordar(p=0){
-    console.log('Engordou')
-    this.peso += p
+let amigo = {
+    nome: 'José',
+    sexo: 'M',
+    peso:85.4,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
 }}
 
 amigo.engordar(2)
