51c58b2a0455355333ec4aaee9e8d1cd6a4e5924
Gil95 <gilsongomesneto.neto@gmail.com>
2023-08-29 23:03:05 -0300

    updated lesson

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 1 - Variáveis/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 1 - Variáveis/script.js
@@ -2,41 +2,53 @@ var nome = 'Gilson';
 let idade = 28;
 const possuiFaculdade = false;
 
-console.log(nome, idade, possuiFaculdade, nome)
+console.log(nome, idade, possuiFaculdade, nome);
 
 let preco = 20;
 var totalComprado = 5;
 var totalPreco = preco * totalComprado;
 
-console.log(nome+' gastou um total de '+totalPreco+' reais no mercado' )
+console.log(nome+' gastou um total de '+totalPreco+' reais no mercado' );
 
-var sobrenome = 'Neto' ,
+var sobrenome = 'Neto',
     cidade = 'Rio';
 
-console.log(sobrenome, cidade)
+console.log(sobrenome, cidade);
 
 var precoAplicativo;
 // retorna undefined
 
 idade = 28;
-console.log(idade)
+console.log(idade);
 idade = 29;
-console.log(idade)
+console.log(idade);
 
 preco = 50;
-console.log(preco)
+console.log(preco);
 preco = 25;
-console.log(preco)
+console.log(preco);
 
 const possuiFaculdade1 = true;
-console.log(possuiFaculdade1)
+console.log(possuiFaculdade1);
 
-var time = 'Vasco'
-console.log(time)
-time = 'Flamengo'
-console.log(time)
+var time = 'Vasco';
+console.log(time);
+time = 'Flamengo';
+console.log(time);
 
-let time1 = 'Vasco 1'
-console.log(time1)
-time1 = 'Flamengo 1'
-console.log(time1)
+let time1 = 'Vasco 1';
+console.log(time1);
+time1 = 'Flamengo 1';
+console.log(time1);
+
+var nome = 'Gilson';
+idade = 28;
+var comidaFavorita;
+comidaFavorita = 'Strogonoff'
+var time = 'Fluminense', 
+    estado,
+    rua,
+    estadoCivil,
+    emprego;
+
+console.log(nome, idade, comidaFavorita, time, estado, rua, estadoCivil, emprego);
