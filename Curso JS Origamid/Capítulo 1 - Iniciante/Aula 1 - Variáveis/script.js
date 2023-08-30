1e1ca737fff2b9c4c56d1b6002655b543ae68ba8
Gil95 <gilsongomesneto.neto@gmail.com>
2023-08-29 22:36:03 -0300

    updated lesson

---
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 1 - Variáveis/script.js
@@ -0,0 +1,42 @@
+var nome = 'Gilson';
+let idade = 28;
+const possuiFaculdade = false;
+
+console.log(nome, idade, possuiFaculdade, nome)
+
+let preco = 20;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+
+console.log(nome+' gastou um total de '+totalPreco+' reais no mercado' )
+
+var sobrenome = 'Neto' ,
+    cidade = 'Rio';
+
+console.log(sobrenome, cidade)
+
+var precoAplicativo;
+// retorna undefined
+
+idade = 28;
+console.log(idade)
+idade = 29;
+console.log(idade)
+
+preco = 50;
+console.log(preco)
+preco = 25;
+console.log(preco)
+
+const possuiFaculdade1 = true;
+console.log(possuiFaculdade1)
+
+var time = 'Vasco'
+console.log(time)
+time = 'Flamengo'
+console.log(time)
+
+let time1 = 'Vasco 1'
+console.log(time1)
+time1 = 'Flamengo 1'
+console.log(time1)
