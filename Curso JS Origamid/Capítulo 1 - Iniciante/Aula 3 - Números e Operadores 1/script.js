896cd07162385f42c580c77ca641edbbbf84004b
Gil95 <gilsongomesneto.neto@gmail.com>
2023-09-05 22:55:49 -0300

    updated lesson

---
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 3 - Números e Operadores 1/script.js
@@ -0,0 +1,49 @@
+var idade = 28;
+
+console.log(idade);
+
+var soma = 100+50;
+console.log(soma);
+
+var subtracao = 100-50;
+console.log(subtracao);
+
+var multiplicacao = 100*2;
+console.log(multiplicacao);
+
+var divisao = 100/2;
+console.log(divisao);
+
+var expoente = 2**4;
+console.log(expoente);
+
+var modulo = 14%5;
+console.log(modulo);
+
+var total = 10+5+10;
+var total2 = 200/5;
+var total3 = 3872983892%3;
+console.log(total);
+console.log(total2);
+console.log(total3);
+
+var soma2 = '100' + 50 ;
+console.log(soma2);
+
+var subtracao2 = '100' - 50;
+console.log(subtracao2);
+
+var multiplicacao2 = '100' * '2';
+console.log(multiplicacao2);
+
+var divisao2 = 'comprei 10' / 2;
+console.log(divisao2);
+
+var testeNaN = 'Isso é 100' / 2;
+console.log(isNaN(testeNaN));
+
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade;
+var pesoPorDois = peso / 2
+console.log(pesoPorDois)
