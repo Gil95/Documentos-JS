c000ab352ac6775ce6c396e678e1149149241184
Gil95 <gilsongomesneto.neto@gmail.com>
2023-09-11 22:31:13 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 3 - Números e Operadores 1/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 3 - Números e Operadores 1/script.js
@@ -45,5 +45,56 @@ console.log(isNaN(testeNaN));
 var numero = 80;
 var unidade = 'kg';
 var peso = numero + unidade;
-var pesoPorDois = peso / 2
-console.log(pesoPorDois)
+var pesoPorDois = peso / 2;
+console.log(pesoPorDois);
+
+var expressao = 20+5*2;
+var expressao2 = (20 + 5) * 2;
+var expressao3 = 20/2*5;
+var expressao4 = 10+10*2+20/2;
+console.log(expressao, expressao2, expressao3, expressao4);
+
+var soma3 = 10+10+20+30*4/2+10;
+console.log(soma3);
+
+var incremento = 5;
+console.log(incremento);
+console.log(++incremento);
+console.log(incremento++);
+console.log(incremento++);
+console.log(incremento++);
+console.log(incremento);
+console.log(--incremento);
+console.log(--incremento);
+
+var frase = 'Isso é um teste';
++frase;
+-frase;
+
+var idade = '28';
+console.log(+idade);
+console.log(-idade);
+console.log(+idade+5);
+
+var possuiFaculdade2 = true;
+console.log(+possuiFaculdade2);
+
+var total4 = 10+5*2/2+20;
+console.log(total4);
+
+var numeralNaN= '24s';
+var fraseNaN = 'Olá';
+console.log(numeralNaN*2);
+console.log(+fraseNaN+2);
+
+var soma4 = '200';
+console.log(+soma4+50);
+
+var incremento2 = 5;
+console.log(++incremento2);
+
+var numero1 = '80';
+var unidade1 = 'kg';
+var peso = numero1 + unidade1;
+var pesoPorDois = numero1 / 2 + unidade1;
+console.log(pesoPorDois);
