2d5b48501b7b6ff92b769080f15c56fd1d3bd16c
Gil95 <gilsongomesneto.neto@gmail.com>
2023-09-13 23:19:54 -0300

    updated lesson

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 4 - Boolean e Condicionais 1/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 4 - Boolean e Condicionais 1/script.js
@@ -20,3 +20,60 @@ if (nome){
 if (!possuiGraduacao){
     console.log('Não possui graduação')
 }
+
+var corFavorita = 'Azul';
+
+switch (corFavorita) {
+    case 'Azul':
+        console.log('Olhe para o céu.');
+        break;
+    case 'Vermelho':
+        console.log('Olhe para as rosas.')
+        break;
+    case 'Amarelo':
+        console.log('Olhe para o sol.');
+        break;
+    case 'Verde':
+        console.log('Olhe para a floresta');
+        break;
+    default:
+        console.log('Feche os olhos.');
+        break;
+}
+
+var minhaIdade = 28;
+var idadeMinhaEsposa = 23;
+
+if (minhaIdade > idadeMinhaEsposa) {
+    console.log('É Maior');
+} else if (minhaIdade ===idadeMinhaEsposa){
+    console.log('É igual');
+} else {
+    console.log('É menor');
+}
+
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expressao)
+
+var brasil = 207;
+var china = 1340;
+
+if (brasil > china) {
+    console.log('É Maior');
+} else if (brasil ===china){
+    console.log('É igual');
+} else {
+    console.log('É menor');
+}
+
+if (('Gato' === 'gato') && ( 5 > 2 )) {
+    console.log('Verdadeiro')
+} else {
+    console.log('Falso')
+}
+
+if (('Gato' ==='gato') || (5 > 2)) {
+    console.log('Gato' && 'Cão')    
+} else {
+    console.log('Falso')
+}
