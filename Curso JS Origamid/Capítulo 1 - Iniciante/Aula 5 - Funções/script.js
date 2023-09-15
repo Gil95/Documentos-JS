7dadcb86c5db7f94af7bdb9a340ad31f9b7e1cf4
Gil95 <gilsongomesneto.neto@gmail.com>
2023-09-14 23:59:50 -0300

    updated lesson

---
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 5 - Funções/script.js
@@ -0,0 +1,33 @@
+function areaQuadrado(lado){
+    return lado * lado;
+}
+
+console.log(areaQuadrado(10));
+
+function pi(){
+    return 3.14;
+}
+
+var total = 5 * pi();
+
+console.log(total);
+
+function imc(peso, altura){
+    const imc = peso / (altura**2);
+    return imc;
+}
+
+console.log(imc (80,1.80));
+console.log(imc(50,1.73));
+
+function corFavorita(cor){
+    if(cor === 'azul'){
+        return 'Você gosta do céu';
+    } else if (cor === 'verde'){
+        return 'Você gosta do mato';
+    } else {
+        return 'Você não gosta de nada';
+    }
+}
+
+console.log(corFavorita('verde'))
