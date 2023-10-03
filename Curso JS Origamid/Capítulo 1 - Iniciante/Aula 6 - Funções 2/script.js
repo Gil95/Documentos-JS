91bb54177b719837c9e78b56ae0f2b570ba7ba6e
Gil95 <gilsongomesneto.neto@gmail.com>
2023-10-02 23:25:37 -0300

    updated project

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 6 - Funções 2/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 6 - Funções 2/script.js
@@ -70,3 +70,15 @@ function tipoDoItem(item){
 addEventListener('click', function (){
     console.log('Gilson Gomes Neto');
 });
+
+var totalPaises = 193;
+function precisoVisitar(paisesVisitados){
+    return `Ainda faltam ${totalPaises-paisesVisitados} países para visitar`;
+}
+
+function jaVisitei(paisesVisitados){
+    return `Já visitei ${paisesVisitados} do total de ${totalPaises}`
+}
+
+console.log (precisoVisitar(20));
+console.log(jaVisitei(10));
---
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 7 - Objetos/script.js
@@ -0,0 +1,10 @@
+var pessoa = {
+    nome: 'Gilson',
+    idade: 28,
+    profissao: 'Estudante de Programação',
+    possuiFaculdade: false,
+}
+
+console.log (pessoa.nome);
+console.log (pessoa.profissao);
+
