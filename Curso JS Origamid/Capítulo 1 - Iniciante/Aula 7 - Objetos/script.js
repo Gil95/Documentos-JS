be734a41bda213273139cb6cdfe3841dd51569d8
Gil95 <gilsongomesneto.neto@gmail.com>
2023-10-18 20:47:36 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 7 - Objetos/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 7 - Objetos/script.js
@@ -8,3 +8,17 @@ var pessoa = {
 console.log (pessoa.nome);
 console.log (pessoa.profissao);
 
+var quadrado = {
+    lados: 4,
+    
+    area: function(lado){
+        return lado*lado;
+    },
+
+    perimetro: function(lado) {
+        return this.lados * lado;
+    }
+}
+
+console.log(quadrado.area(5))
+console.log(quadrado.perimetro(5))
