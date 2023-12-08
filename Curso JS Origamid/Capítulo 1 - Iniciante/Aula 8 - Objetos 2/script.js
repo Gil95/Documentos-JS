1db399bfadff2dc3f53d320c2044e4c9ae331e10
Gil95 <gilsongomesneto.neto@gmail.com>
2023-12-07 22:20:57 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 8 - Objetos 2/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 8 - Objetos 2/script.js
@@ -2,6 +2,9 @@ var menu = {
     width: 800,
     height: 50,
     backgroundColor: '#84E',
+    metadeHeight(){
+        return this.height/2;
+    }
 }
 
 menu.backgroundColor = '#000';
@@ -12,3 +15,28 @@ menu.esconder = function(){
 }
 
 var bg = menu.backgroundColor;
+
+var estudante = {
+    nome: 'Gilson',
+    sobrenome: 'Neto',
+    idade: 28,
+    areaDeEstudo: 'Front-End',
+}
+
+console.log(estudante.nomeCompleto)
+var carro = {
+    preco: 32000,
+    portas: 2,
+    marca: 'Mitsubishi'
+}
+
+carro.preco = 35000;
+
+var cachorro = {
+    raca: 'labrador',
+    cor: 'preto',
+    idade: 10,
+    if(viuHomem){
+        console.log('latir')
+    },
+}
