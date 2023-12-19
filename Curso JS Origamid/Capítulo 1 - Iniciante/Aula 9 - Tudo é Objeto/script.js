ae332cf1fb1d2493d1f55f679527294872337be4
Gil95 <gilsongomesneto.neto@gmail.com>
2023-12-18 23:25:14 -0300

    updated lessons

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 9 - Tudo é Objeto/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 9 - Tudo é Objeto/script.js
@@ -7,3 +7,31 @@ function areaQuadrado(lado){
 }
 
 var btn = document.querySelector('.btn')
+
+var profissao = 'Estudante'
+profissao.endsWith
+profissao.length
+profissao.slice
+
+var btn = document.querySelector('.btn');
+btn.addEventListener
+btn.appendChild
+btn.innerHTML
+btn.id
+btn.outerHTML
+
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
+
