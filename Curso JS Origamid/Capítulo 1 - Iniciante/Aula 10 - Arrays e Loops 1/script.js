94de18bf15e392c79d9d2a73e88a2105e1466bd7
Gil95 <gilsongomesneto.neto@gmail.com>
2023-12-20 23:37:15 -0300

    updated lesson

---
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 10 - Arrays e Loops 1/script.js
@@ -0,0 +1,18 @@
+var videoGames = ['Switch' , 'PS4', 'XBOX'];
+
+console.log(videoGames[0]) //Switch
+console.log(videoGames[2]) //XBox
+videoGames.push('3DS')
+videoGames.length;
+
+var ultimoItem = videoGames.pop()
+
+for (var numero = 1; numero < 10 ; numero++){
+    console.log(numero);
+} 
+
+var i = 0;
+while (i <= 10) {
+    console.log(i);
+    i = i + 5;
+}
