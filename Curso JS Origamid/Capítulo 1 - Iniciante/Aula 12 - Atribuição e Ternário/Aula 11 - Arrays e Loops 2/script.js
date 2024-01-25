ecb81ef648b20b92a215dfcb83265fa98d83ac92
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-01-24 22:13:26 -0300

    updated lesson

---
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 12 - Atribuição e Ternário/Aula 11 - Arrays e Loops 2/script.js
@@ -0,0 +1,16 @@
+var x = 5;
+var y = 10;
+
+//console.log(x += y)
+//console.log(x -= y)
+//console.log(x *= y)
+//console.log(x /= y)
+//console.log(x %= y)
+//console.log(x **= y)
+
+var numero = 20;
+var numero2 = 10;
+
+numero /= numero2;
+console.log(numero);
+console.log(numero2)
