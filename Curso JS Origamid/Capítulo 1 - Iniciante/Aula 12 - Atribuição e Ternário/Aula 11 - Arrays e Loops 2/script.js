c742c07a6d6b268e409c90a61aa0b9e813c54bb4
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-01-26 00:47:18 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 12 - Atribuição e Ternário/Aula 11 - Arrays e Loops 2/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 12 - Atribuição e Ternário/Aula 11 - Arrays e Loops 2/script.js
@@ -1,12 +1,13 @@
 var x = 5;
 var y = 10;
 
-//console.log(x += y)
-//console.log(x -= y)
-//console.log(x *= y)
-//console.log(x /= y)
-//console.log(x %= y)
-//console.log(x **= y)
+//operadores de atribuição
+//console.log(x += y) x = x+y
+//console.log(x -= y) x = x-y
+//console.log(x *= y) x = x*y
+//console.log(x /= y) x = x/y
+//console.log(x %= y) x = x%y
+//console.log(x **= y) x = x**y
 
 var numero = 20;
 var numero2 = 10;
@@ -14,3 +15,12 @@ var numero2 = 10;
 numero /= numero2;
 console.log(numero);
 console.log(numero2);
+
+//Ternário
+var idade = 20;
+var naoPossuiDiabetes = true;
+
+var podeBeber;
+podeBeber = idade >= 18 && naoPossuiDiabetes? true: false;
+
+console.log(podeBeber)
