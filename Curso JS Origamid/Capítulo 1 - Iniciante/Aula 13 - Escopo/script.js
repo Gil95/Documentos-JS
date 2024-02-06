3176c8caff0ada3d2aac9346aafcded0c8e1c497
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-02-05 23:32:02 -0300

    updated lessons

--- Curso JS Origamid/Capítulo 1 - Iniciante/Aula 13 - Escopo/script.js
+++ Curso JS Origamid/Capítulo 1 - Iniciante/Aula 13 - Escopo/script.js
@@ -21,3 +21,50 @@ if(true){
     let carro2 = 'Pajero';
     console.log(carro2);
 }
+
+for(let i = 0; i < 10; i++){
+    console.log(`Número ${i}`);
+}
+
+const semana = 'Sexta';
+console.log(semana);
+
+const data = {
+    ano: 2018,
+    mes:'Dezembro',
+}
+
+console.log(data);
+console.log(data.mes);
+
+data.dia = 29;
+
+console.log(data);
+
+var cor = 'preto';
+const marca = 'Fiat';
+let portas = 4;
+
+console.log(/*var,*/ marca, portas);
+
+const dois = 2;
+
+function somarDois(x){
+    return x + dois;
+}
+
+function dividirDois(x){
+    return x / dois;
+}
+
+console.log(somarDois(4));
+console.log(dividirDois(28));
+
+const numero = 50;
+
+for (let numero = 0; numero <10; numero++){
+    console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
