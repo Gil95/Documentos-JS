d7dd88f6a3241ff8d20b56126f8e84200c598036
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-05-16 22:36:59 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 3 - Seleção de Elementos/script.js
+++ Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 3 - Seleção de Elementos/script.js
@@ -22,3 +22,12 @@ console.log(linkInterno);
 
 const animaisImg = document.querySelectorAll('.animais img');
 console.log(animaisImg);
+
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+primeiroUl.classList.add('grid-section');
+
+console.log(gridSectionHTML);
+console.log(gridSectionNode);
