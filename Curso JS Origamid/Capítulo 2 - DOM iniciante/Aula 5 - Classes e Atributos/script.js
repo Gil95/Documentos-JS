eb007dfb28cb80a3ca5e81b2aa096967f9d6fccf
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-06-07 00:16:34 -0300

    updated lesson

--- Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 5 - Classes e Atributos/script.js
+++ Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 5 - Classes e Atributos/script.js
@@ -10,3 +10,33 @@ menu.classList.contains('ativo'); //true ou false
 menu.classList.replace('ativo', 'inativo');
 
 console.log(menu.classList.contains('ativo'));
+
+if(menu.classList.contains('azul')){
+    menu.classList.add('possui-azul');
+} else {
+    menu.classList.add('nao-possui-azul');
+}
+
+menu.className += ' vermelho';
+
+console.log(menu.className);
+
+const animais = document.querySelector('.animais');
+
+console.log(animais.attributes['data-texto']);
+
+const img = document.querySelector('img');
+
+console.log(img.getAttribute('src'));
+
+console.log(img.setAttribute('alt', 'Texto Alternativo'));
+
+console.log(img.hasAttribute('id'));
+
+console.log(img.getAttribute('alt'));
+
+img.removeAttribute('alt');
+
+img.hasAttributes();
+
+console.log(img.getAttribute('alt'));
