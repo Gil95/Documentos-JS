b320ea8c30321b46d3b26af9492255c1bc8ea9b1
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-06-25 23:18:40 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 7 - Eventos/exercícios/script.js
+++ Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 7 - Eventos/exercícios/script.js
@@ -1,14 +1,8 @@
-const linkInterno = document.querySelector('a[href^="#"]');
+const linkInterno = document.querySelectorAll('a[href^="#"]');
 
-function handleClick(event) {
-    if(event.key === 'f')
-        document.body.classList.toggle('fullscreen');
-    else if (event.key === 'v'){
-        document.body.classList.toggle('vermelho');
-        document.body.classList.remove('azul');}
-    else if (event.key === 'a'){
-        document.body.classList.toggle('azul');
-        document.body.classList.remove('vermelho');}
-}
+function handleClick(event){
+        event.preventDefault();
+        document.body.classList.toggle('ativo');
+};
 
 linkInterno.addEventListener('click', handleClick);
