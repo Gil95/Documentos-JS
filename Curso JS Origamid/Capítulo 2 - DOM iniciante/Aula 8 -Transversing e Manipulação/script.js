d6b4c39a725adfa5e1138505c0fe0372b80c187f
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-07-08 21:26:53 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 8 -Transversing e Manipulação/script.js
+++ Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 8 -Transversing e Manipulação/script.js
@@ -1,11 +1,22 @@
-const titulo = document.querySelector('h1');
+// const titulo = document.querySelector('h1');
 
-console.log(titulo.outerHTML);
-console.log(titulo.innerHTML);
-console.log(titulo.innerText);
+// console.log(titulo.outerHTML);
+// console.log(titulo.innerHTML);
+// console.log(titulo.innerText);
 
-const animaisLista = document.querySelector('.animais-lista');
+// const animaisLista = document.querySelector('.animais-lista');
 
-console.log(animaisLista.outerHTML);
-console.log(animaisLista.innerHTML);
-console.log(animaisLista.innerText);
+// console.log(animaisLista.outerHTML);
+// console.log(animaisLista.innerHTML);
+// console.log(animaisLista.innerText);
+
+const h1 = document.querySelector('h1');
+const animaisDescricao = document.querySelector('.animais-descricao');
+
+console.log(h1.innerHTML);
+console.log(h1.outerHTML);
+
+h1.innerHTML = '<p>Novo Título</p>';
+
+console.log(h1.innerHTML);
+console.log(h1.outerHTML);
