5ccb1b1b1ec4cdce2980b53b238fb7b7971fdd57
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-08-14 20:47:16 -0300

    updated lesson

--- Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 9 - Navegação por Tabs/script.js
+++ Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 9 - Navegação por Tabs/script.js
@@ -1,5 +1,9 @@
 const tabMenu = document.querySelectorAll('.js-tabmenu li');const tabContent = document.querySelectorAll('.js-tabcontent section');
 
+if(tabMenu.length && tabContent.length){
+    tabContent[0].classList.add('ativo');
+}
+    
 function activeTab(index) {
     tabContent.forEach((section) => {
         section.classList.remove('ativo');
