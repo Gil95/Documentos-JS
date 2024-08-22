5e3f317b1eff31c885faed1c3f0ab778749d7215
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-08-22 00:05:09 -0300

    updated lesson

--- Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 10 - Accordion List/script.js
+++ Curso JS Origamid/Capítulo 2 - DOM iniciante/Aula 10 - Accordion List/script.js
@@ -1,18 +1,35 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav(){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabMenu.length && tabContent.length){
-    tabContent[0].classList.add('ativo');
-}
-    
-function activeTab(index) {
-    tabContent.forEach((section) => {
-        section.classList.remove('ativo');
-    });
-    tabContent[index].classList.add('ativo')
+    if(tabMenu.length && tabContent.length){
+        tabContent[0].classList.add('ativo');
+
+
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
+        });
+    }
 }
 
-tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    })
-});
+initTabNav();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion() {
+    this.classList.add('ativo');
+    this.nextElementSibling.classList.add('ativo');
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+})
