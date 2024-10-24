091b00e98ba722dafcf5199853005ceb485ac0c6
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-10-23 21:27:45 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 3 - Objetos/Aula 1 - Constructor Function/exercícios/script.js
+++ Curso JS Origamid/Capítulo 3 - Objetos/Aula 1 - Constructor Function/exercícios/script.js
@@ -27,6 +27,30 @@ const bruno = new Pessoa('Bruno', 15);
 
 
 // Crie uma Constructor Function (Dom) para manipulação de listas de elementos dom. Deve conter as seguintes propriedades e métodos:
-/* Elements , retorna NodeList com os elementos selecionados:
+
+/*
+elements , retorna NodeList com os elementos selecionados
 addClass(classe), adiciona a classe a todos os elementos
-removeClass()classe, remove a classe a todos os elementos */
+removeClass()classe, remove a classe a todos os elementos
+*/
+
+function Dom(seletor) {
+    const elementList = document.querySelectorAll(seletor);
+    this.elements = elementList;
+    this.addClass = function(classe) {
+        elementList.forEach((element) => {
+            element.classList.add(classe);
+        })
+    }
+    this.removeClass = function(classe) {
+        elementList.forEach((element) => {
+            element.classList.remove(classe);
+        })
+    }
+}
+
+const listaItens = new Dom('li');
+const ul = new Dom('ul');
+
+listaItens.addClass('ativar');
+ul.addClass('ativar-ul');
