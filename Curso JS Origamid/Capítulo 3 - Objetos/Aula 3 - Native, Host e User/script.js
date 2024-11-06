1c74a7e4be6b2c727bc022d9419d2062a01ebabc
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-11-06 01:36:39 -0300

    updated lesson

---
+++ Curso JS Origamid/Capítulo 3 - Objetos/Aula 3 - Native, Host e User/script.js
@@ -0,0 +1,16 @@
+// Contrutores de objetos nativos
+Object
+String
+Array
+Function
+
+//Objetos do browser
+NodeList
+HTMLCollection
+Element
+
+//Objetos do usuário = Objetos criados
+const Pessoa = {
+    nome: 'Gilson',
+    idade: 29,
+}
