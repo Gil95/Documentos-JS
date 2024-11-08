e2604ce8e521da867774f698c7c842c803f871bd
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-11-07 23:55:20 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 3 - Objetos/Aula 3 - Native, Host e User/script.js
+++ Curso JS Origamid/Capítulo 3 - Objetos/Aula 3 - Native, Host e User/script.js
@@ -14,3 +14,9 @@ const Pessoa = {
     nome: 'Gilson',
     idade: 29,
 }
+
+if(typeof Array.from.sijdnajsn !== "undefined"){
+    console.log('Existe');
+} else {
+    console.log('Não existe');
+}
