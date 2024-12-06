1582a3ce0af62ea8a79f8c145bdaa47fcdb1f9db
Gilson Neto <gilsongomesneto.neto@gmail.com>
2024-12-05 23:13:55 -0300

    Update script.js

--- Curso JS Origamid/Capítulo 3 - Objetos/Aula 4 - String/script.js
+++ Curso JS Origamid/Capítulo 3 - Objetos/Aula 4 - String/script.js
@@ -30,3 +30,18 @@ console.log(fruta.includes(listaFrutas));
 console.log(fruta.endsWith('nana'));
 console.log(fruta.startsWith('Ba'));
 console.log(fruta.startsWith('na'));
+
+const transacao1 = 'Depósito de cliente';
+const transacao2 = 'Depósito de fornecedor';
+const transacao3 = 'Taxa de camisas';
+
+console.log(transacao1.slice(0,3));
+console.log(transacao2.slice(0,3));
+console.log(transacao3.slice(0,3));
+
+console.log(transacao1.slice(12));
+console.log(transacao1.slice(-4));
+console.log(transacao1.slice(3, 6));
+
+console.log(fruta.slice(-3));
+
