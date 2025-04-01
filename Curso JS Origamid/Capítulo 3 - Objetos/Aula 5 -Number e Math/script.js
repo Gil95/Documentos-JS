1edff10d94a0b65b9fc21f1bbc784560f5569fe6
Gilson Neto <gilsongomesneto.neto@gmail.com>
2025-03-31 21:47:41 -0300

    updated lesson

--- Curso JS Origamid/Capítulo 3 - Objetos/Aula 5 -Number e Math/script.js
+++ Curso JS Origamid/Capítulo 3 - Objetos/Aula 5 -Number e Math/script.js
@@ -3,6 +3,7 @@ const preco = new Number(99);
 
 console.log(Number.isNaN(NaN));
 console.log(Number.isNaN(4+5));
+console.log(Number.isNaN('Olá, Mundo!'));
 console.log(Number.isInteger(20));
 console.log(Number.isInteger(23.6));
-
+console.log(parseFloat('99.50'))
