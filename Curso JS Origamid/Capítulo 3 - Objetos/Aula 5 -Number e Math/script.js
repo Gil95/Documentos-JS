81cdf8c8f7ebc583e0add80c4299eeea947e6f31
Gilson Neto <gilsongomesneto.neto@gmail.com>
2025-04-01 01:20:51 -0300

    updated lesson

--- Curso JS Origamid/Capítulo 3 - Objetos/Aula 5 -Number e Math/script.js
+++ Curso JS Origamid/Capítulo 3 - Objetos/Aula 5 -Number e Math/script.js
@@ -6,4 +6,12 @@ console.log(Number.isNaN(4+5));
 console.log(Number.isNaN('Olá, Mundo!'));
 console.log(Number.isInteger(20));
 console.log(Number.isInteger(23.6));
-console.log(parseFloat('99.50'))
+
+console.log(parseFloat('99.50'));
+console.log(Number.parseFloat('99,50'));
+console.log(Number.parseFloat('100 Reais'));
+console.log(Number.parseFloat('R$100,00'));
+
+console.log(parseInt('99,50'));
+console.log(parseInt(5.434343555555, 10));
+console.log(Number.parseInt('100 Reais'));
