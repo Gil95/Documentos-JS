aa271647022a6df1054fb482e90dad2f7f07a122
Gilson Neto <gilsongomesneto.neto@gmail.com>
2026-01-20 19:30:39 -0300

    updated lesson

--- Curso JS Origamid/Capítulo 3 - Objetos/Aula 7 - Array e Iteração/script.js
+++ Curso JS Origamid/Capítulo 3 - Objetos/Aula 7 - Array e Iteração/script.js
@@ -12,8 +12,8 @@ carros.forEach((item, index, array) => {
 const carros2 = ['Ford', 'Fiat', 'Honda']
 carros2.forEach((item, index, array) => {
     array[index] = item.toUpperCase();
-    console.log(item.toUpperCase(), index, array);
 });
+console.log(carros2);
 
 // Arrow Function
 
@@ -25,3 +25,33 @@ li.forEach(function(item, index){
     item.classList.add('ativa' + index);
 });
 
+// Modificar a Array Original
+
+const carros3 = ['Ford', 'Fiat', 'Honda'];
+carros3.forEach((item, index, array) => {
+    array[index] = 'Carro ' + item;
+});
+console.log(carros3);
+
+// [].map()
+
+const carros4 = ['Ford', 'Fiat', 'Honda'];
+
+const newCarros = carros4.map((item) => {
+    return 'Carro '+ item;
+});
+
+console.log(carros4, newCarros);
+
+const retornoForeach = li.forEach((item, index) => {
+    item.classList.add('ativa' + index)
+});
+
+console.log(retornoForeach);
+console.log(li);
+
+const novaArray = carros4.map((item, index, array) => {
+    return item.toUpperCase(); //Funções não dão retorno, apenas mudam as variáveis, caso queira um return, deve exigir um return para a mesma.
+});
+
+console.log(novaArray, carros4);
